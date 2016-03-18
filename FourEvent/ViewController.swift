246dbef8b76b16ccbfcf3834874729e753e593b9
Valentina <Vale@valentinas-macbook-pro.local>
2016-03-18 21:23:57 +0100

    Initial Commit

---
+++ FourEvent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourEvent
+//
+//  Created by Valentina on 18/03/16.
+//  Copyright © 2016 Valentina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
